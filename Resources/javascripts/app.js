23891c4959fcefe73fb1e2a9bd1ae30102773298
Justin Gitlin <cacheflowe@cacheflowe.com>
2011-12-30 14:29:38 -0700

    refactored the app into a .js class and got proper initialization of menu items working based on stored prefs

--- Resources/javascripts/app.js
+++ Resources/javascripts/app.js
@@ -1,91 +1,154 @@
-// Config
-// ------------------------------------------------------------------------
-var _minute = 60 * 1000;
-var _stretchInterval = _minute * 0.1;
+var StretchTimer = (function(){
+	// Config
+	// ------------------------------------------------------------------------
+	// timer vars
+	var _minuteMillies = 60 * 1000,
+		_timerInterval = null,
+		_numStretchesIgnored = 0,
+		_numIntervalOptions = 0,
+		_intervalMenu = null,
+	
+	// App objects and vars
+		_stretches = null,
+		_numStretches = 0,
+		_curStretchIndex = 0,
+		_stretchImg = $('img#stretch_img'),
+		_imgPath = 'images/stretch/',
+		
+	// audio vars
+		_pingSound = Titanium.Media.createSound('app://sounds/ping.wav'),
+	
+	// constants
+		TIMER_MINUTES = 'interval',
+		AUDIO_ENABLED = 'audio_enabled';
+		
+	// Kick it off
+	// ------------------------------------------------------------------------
+	var init = function(){
+		retrieveSettings();
+		buildStretches();
+		setCurStretch();
+		buildMenu();
+		disableImageDragging();
+		bindKeys();
+		bindAppEvents();
+		
+		// Kick off initial timer
+		setNewTimer( parseInt( localStorage.getItem( TIMER_MINUTES ) ) );
+	};
+	
+	// Retrieve stored settings or set defaults
+	// ------------------------------------------------------------------------
+	var retrieveSettings = function(){
+		if( localStorage.getItem( TIMER_MINUTES ) == null ) localStorage.setItem( TIMER_MINUTES , 15); 
+		if( localStorage.getItem( AUDIO_ENABLED ) == null ) localStorage.setItem( AUDIO_ENABLED , false); 
+	};
+	
+	// Build stretches array
+	// ------------------------------------------------------------------------
+	var buildStretches = function(){
+		_stretches = [
+			{image:'arm_circles.png', desc:'arm circles'},
+			{image:'arm_stretch.png', desc:'arm stretch'},
+			{image:'back_stretch.png', desc:'back stretch'},
+			{image:'calf_stretch.png', desc:'calf stretch'},
+			{image:'elbow_press.png', desc:'elbow press'},
+			{image:'finger_stretch.png', desc:'finger stretch'},
+			{image:'head_tilt.png', desc:'head tilt'},
+			{image:'shoulder_shrug.png', desc:'shoulder shrug'},
+			{image:'shoulder_stretch.png', desc:'shoulder stretch'},
+			{image:'wholearm_stretch.png', desc:'whole arm stretch'},
+			{image:'wrist_warmup.png', desc:'wrist warmup'}
+		];
+		_numStretches = _stretches.length;
+		_curStretchIndex = Math.round( Math.random() * _numStretches );
+	};
+	
+	
+	// Update stretch image
+	// ------------------------------------------------------------------------
+	var setCurStretch = function(){
+		_stretchImg.attr( 'src', _imgPath + _stretches[_curStretchIndex].image );
+	};
+	
+	// Set up keyboard commands
+	// ------------------------------------------------------------------------
+	var bindKeys = function(){
+		$(document).keyup(function(e) {
+			// quit app on ESC
+			if (e.keyCode == 27) { Titanium.App.exit(); }
+		});
+	};
+	
+	// Listen for Titanium app events
+	// ------------------------------------------------------------------------
+	var bindAppEvents = function(){
+		// Listen for window focus event to clear the badge
+		Titanium.API.addEventListener(Titanium.FOCUSED, function(event) {
+			clearBadge();
+		});
+	};
 
-// Stretches array
-// ------------------------------------------------------------------------
-var _stretches = [
-	{image:'arm_circles.png', desc:'arm circles'},
-	{image:'arm_stretch.png', desc:'arm stretch'},
-	{image:'back_stretch.png', desc:'back stretch'},
-	{image:'calf_stretch.png', desc:'calf stretch'},
-	{image:'elbow_press.png', desc:'elbow press'},
-	{image:'finger_stretch.png', desc:'finger stretch'},
-	{image:'head_tilt.png', desc:'head tilt'},
-	{image:'shoulder_shrug.png', desc:'shoulder shrug'},
-	{image:'shoulder_stretch.png', desc:'shoulder stretch'},
-	{image:'wholearm_stretch.png', desc:'whole arm stretch'},
-	{image:'wrist_warmup.png', desc:'wrist warmup'}
-];
-var _numStretches = _stretches.length;
-var _curStretchIndex = Math.round( Math.random() * _numStretches );
-var _stretchImg = $('img#stretch_img');
-var _imgPath = 'images/stretch/';
-
-var setCurStretch = function(){
-	_stretchImg.attr( 'src', _imgPath + _stretches[_curStretchIndex].image );
-};
-setCurStretch();
-
-
-// close app on ESC
-// ------------------------------------------------------------------------
-$(document).keyup(function(e) {
-	if (e.keyCode == 27) { Titanium.App.exit(); }   // esc
-});
-
-// disable clicking and dragging image
-// ------------------------------------------------------------------------
-var recurseDisableImages = function ( elem ) {
-	if( elem ) {
-		// disable clicking/dragging
-		if( elem.tagName && elem.tagName.toLowerCase() == 'img' ) {
-			try {
-				elem.onmousedown = function(e){ return false; };
-				elem.onselectstart = function(){ return false; };
-			} catch(err) {}
-		}
-		// loop through children and do the same
-		if( elem.childNodes.length > 0 ){
-			for( var i=0; i < elem.childNodes.length; i++ ) {
-				this.recurseDisableImages( elem.childNodes[i] );
-			}
-		}
-	}
-};
-recurseDisableImages( document.getElementById("outer") );
-
-// update on interval
-// ------------------------------------------------------------------------
-var _timerInterval = null;
-var _numStretchesIgnored = 0;
-var _pingSound = Titanium.Media.createSound('app://sounds/ping.wav');
-var _soundOn = localStorage.getItem('sound_enabled') || false;
-
-var clearBadge = function() {
-	_numStretchesIgnored = 0;
-	Titanium.UI.setBadge('');
-}
-
-var setNewTimer = function( minutes ){
-	// set correct highlight
-	setTimeout(function(){
-		for (var i=0; i < numIntervals; i++) {
-			console.log('minutes = '+minutes);
-			var label = intervalMenu.getItemAt(i).getLabel();
-			if( parseInt( label.split(' ')[0] ) == minutes ) {
-				intervalMenu.getItemAt(i).setState( true );
-			} else {
-				intervalMenu.getItemAt(i).setState( false );
+	// Disable clicking and dragging image
+	// ------------------------------------------------------------------------
+	var disableImageDragging = function(){
+		var recurseDisableImages = function ( elem ) {
+			if( elem ) {
+				// disable clicking/dragging
+				if( elem.tagName && elem.tagName.toLowerCase() == 'img' ) {
+					try {
+						elem.onmousedown = function(e){ return false; };
+						elem.onselectstart = function(){ return false; };
+					} catch(err) {}
+				}
+				// loop through children and do the same
+				if( elem.childNodes.length > 0 ){
+					for( var i=0; i < elem.childNodes.length; i++ ) {
+						recurseDisableImages( elem.childNodes[i] );
+					}
+				}
 			}
 		};
-	},10);
+		recurseDisableImages( $("div#outer")[0] );
+	};
 	
-	// clear & kick off new timer
-	_stretchInterval = _minute * minutes;
-	if( _timerInterval ) clearInterval( _timerInterval );
-	_timerInterval = setInterval(function(){
+	// Clear the app badge in the dock
+	// ------------------------------------------------------------------------	
+	var clearBadge = function() {
+		_numStretchesIgnored = 0;
+		Titanium.UI.setBadge('');
+	};
+	
+	// Set the checkbox state on the selected Interval menu item
+	// ------------------------------------------------------------------------	
+	var updateIntervalMenu = function( minutes ){
+		setTimeout(function(){
+			for (var i=0; i < _numIntervalOptions; i++) {
+				var label = _intervalMenu.getItemAt(i).getLabel();
+				if( parseInt( label.split(' ')[0] ) == minutes ) {
+					_intervalMenu.getItemAt(i).setState( true );
+				} else {
+					_intervalMenu.getItemAt(i).setState( false );
+				}
+			};
+		},10);	// has to happen just after the 'native' response of the checkbox or it gets cleared out
+	};
+		
+	// Clear the timer and reset based on the new timer interval
+	// ------------------------------------------------------------------------	
+	var setNewTimer = function( minutes ){
+		// store selection and update menu 
+		updateIntervalMenu( minutes );
+		// clear & kick off new timer
+		if( _timerInterval ) clearInterval( _timerInterval );
+		_timerInterval = setInterval(function(){
+			incrementStretch();
+		}, _minuteMillies * minutes );
+	};
+
+	// Increment the stretch
+	// ------------------------------------------------------------------------
+	var incrementStretch = function(){
 		// increment stretch
 		_curStretchIndex++;
 		if( _curStretchIndex >= _numStretches ) _curStretchIndex = 0;
@@ -97,10 +160,7 @@ var setNewTimer = function( minutes ){
 		_numStretchesIgnored++;
 		Titanium.UI.setBadge( ''+_numStretchesIgnored );
 		// Titanium.Media.beep();
-		if( _soundOn ) _pingSound.play();
-		
-		// focus the window
-		// Titanium.UI.getMainWindow().setTopMost();
+		if( localStorage.getItem( AUDIO_ENABLED ) == 'true' ) _pingSound.play();
 		
 		// clear the badge when clicked
 		var notification = Titanium.Notification.createNotification({
@@ -111,65 +171,48 @@ var setNewTimer = function( minutes ){
 			'icon': 'app://' + _imgPath + _stretches[_curStretchIndex].image
 		});
 		notification.show();
-	},_stretchInterval);
-};
-
-
-// Listen for window focus event
-// ------------------------------------------------------------------------
-Titanium.API.addEventListener(Titanium.FOCUSED, function(event) {
-	clearBadge();
-});
-
-
-
-// Build app menu
-// ------------------------------------------------------------------------
-
-// Create main menu
-var mainMenu = Titanium.UI.createMenu();
-
-// create interval setting
-mainMenu.addItem("Interval");
-
-var intervalMenu = Titanium.UI.createMenu();
-var numIntervals = 0;
-for( var i=5; i < 40; i+=5 ){
-	(function(){
-		var minutes = i;
-		intervalMenu.appendItem( Titanium.UI.createCheckMenuItem(minutes+' minutes', function(){
-			setNewTimer( minutes );
-		}) );
-	})();
-	numIntervals++;
-}
-mainMenu.getItemAt(0).setSubmenu(intervalMenu);
-
-// Create sound menu with audio toggle
-mainMenu.addItem("Sound");
-
-var audioMenu = Titanium.UI.createMenu();
-mainMenu.getItemAt(1).setSubmenu( audioMenu );
-var checkItem = Titanium.UI.createCheckMenuItem("On", function() {
-	_soundOn = !checkItem.getState();
-	localStorage.setItem('sound_enabled', _soundOn);
-});
-// checkItem.setState(_soundOn);
-audioMenu.appendItem(checkItem);
-
-// Add main menu items 
-Titanium.UI.setMenu(mainMenu);
-
-
-// Retrieve settings
-// ------------------------------------------------------------------------
-if( localStorage.getItem('interval') == null ) {
-	localStorage.setItem('interval', 15); 
-} else {
+	};
 	
-}
-
-
-// Kick off initial timer
-// ------------------------------------------------------------------------
-setNewTimer( 15 );
+	// Build app menu
+	// ------------------------------------------------------------------------
+	var buildMenu = function(){
+		// Create main menu
+		var mainMenu = Titanium.UI.createMenu();
+		
+		// create interval setting
+		mainMenu.addItem("Interval");
+		
+		_intervalMenu = Titanium.UI.createMenu();
+		for( var i=5; i < 40; i+=5 ){
+			(function(){
+				var minutes = i;
+				_intervalMenu.appendItem( Titanium.UI.createCheckMenuItem(minutes+' minutes', function(){
+					localStorage.setItem( TIMER_MINUTES, minutes );
+					setNewTimer( minutes );
+				}) );
+			})();
+			_numIntervalOptions++;
+		}
+		mainMenu.getItemAt(0).setSubmenu( _intervalMenu );
+		
+		// Create sound menu with audio toggle
+		mainMenu.addItem("Sound");
+		
+		var audioMenu = Titanium.UI.createMenu();
+		var checkItem = Titanium.UI.createCheckMenuItem("On", function() {
+			var isSelected = !checkItem.getState();
+			localStorage.setItem( AUDIO_ENABLED, isSelected );
+		});
+		mainMenu.getItemAt(1).setSubmenu( audioMenu );
+		
+		audioMenu.appendItem(checkItem);
+		// Add main menu items 
+		Titanium.UI.setMenu(mainMenu);
+		
+		// set checked state based on stored prefs
+		( localStorage.getItem( AUDIO_ENABLED ) == 'true' ) ? checkItem.setState( true ) : checkItem.setState( false );
+		updateIntervalMenu( parseInt( localStorage.getItem( TIMER_MINUTES ) ) );
+	};
+	
+	init();
+})();
